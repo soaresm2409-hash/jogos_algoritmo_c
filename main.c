61e0911b9ac9f048b21d96ada4cf15a77553c717
emanuelly soares <soaresm2409@gmail.com>
2026-03-26 11:44:53 -0300

    main.c

--- main.c
+++ main.c
@@ -39,7 +39,9 @@ int jogoGousmasWar();
 }
 
 // JOGO 1: PERGUNTA E RESPOSTA 
+
 int jogoPerguntaResposta() {
+	
     int jogarNovamente;
     do {
         printf("\n--- JOGO 1: CAPITAIS DO MUNDO ---\n");
@@ -79,90 +81,314 @@ int jogoPerguntaResposta() {
 }
 
 // JOGO 2: COBRA NA CAIXA 
-int jogoCobraNaCaixa() {
+   int jogoCobraNaCaixa() 
+{
     int jogarNovamente;
     srand(time(NULL));
-    do {
+    do 
+    {
         printf("\n--- JOGO 2: COBRA NA CAIXA! ---\n");
-        int c1=0, c2=0, c3=0, c4=0, c5=0; // 0=fechada, 1=aberta
-        int pBotao, pCobra, escolha, rodando=1, turno=1;
+        int c1 = 0;
+        int c2 = 0;
+        int c3 = 0;
+        int c4 = 0;
+        int c5 = 0;
+        int pBotao;
+        int pCobra;
+        int escolha;
+        int rodando = 1; 
+        int turno = 1;
 
         pBotao = (rand() % 5) + 1;
-        do { pCobra = (rand() % 5) + 1; } while (pCobra == pBotao);
+        do 
+        { 
+            pCobra = (rand() % 5) + 1; 
+        } 
+        while (pCobra == pBotao);
 
-        while (rodando == 1) {
-            printf("\nTurno Jogador %d. Caixas: ", (turno % 2 == 0) ? 2 : 1);
-            printf("%s %s %s %s %s", c1?"[X]":"[1]", c2?"[X]":"[2]", c3?"[X]":"[3]", c4?"[X]":"[4]", c5?"[X]":"[5]");
+        while (rodando == 1) 
+        {
+            int quemJoga;
+
             
-            printf("\nEscolha (1-5): ");
+            if (turno % 2 == 0) 
+            {
+                quemJoga = 2;
+            } 
+            else 
+            {
+                quemJoga = 1;
+            }
+            printf("\nTurno Jogador %d.", quemJoga);
+            printf("\nStatus das Caixas: ");
+
+            if (c1 == 1) 
+            {
+                printf("[X] ");
+            } 
+            else 
+            {
+                printf("[1] ");
+            }
+            if (c2 == 1) 
+            {
+                printf("[X] ");
+            } 
+            else 
+            {
+                printf("[2] ");
+            }
+            if (c3 == 1) 
+            {
+                printf("[X] ");
+            } 
+            else 
+            {
+                printf("[3] ");
+            }
+            if (c4 == 1) 
+            {
+                printf("[X] ");
+            } 
+            else 
+            {
+                printf("[4] ");
+            }
+            if (c5 == 1) 
+            {
+                printf("[X] ");
+            } 
+            else 
+            {
+                printf("[5] ");
+            }
+
+            printf("\nEscolha uma caixa (1-5): ");
             scanf("%d", &escolha);
 
             int valida = 0;
-            if (escolha==1 && c1==0) { c1=1; valida=1; }
-            else if (escolha==2 && c2==0) { c2=1; valida=1; }
-            else if (escolha==3 && c3==0) { c3=1; valida=1; }
-            else if (escolha==4 && c4==0) { c4=1; valida=1; }
-            else if (escolha==5 && c5==0) { c5=1; valida=1; }
 
-            if (valida == 1) {
-                if (escolha == pBotao) { printf("VITORIA!Achou o botao!"); rodando=0; }
-                else if (escolha == pCobra) { printf("DERROTA!A cobra te pegou!"); rodando=0; }
-                else { printf("Vazia..."); turno++; }
-            } else printf("Invalida!");
-        }
-        printf("\n1: Jogar de novo | 0: Menu: ");
+            if (escolha == 1 && c1 == 0) 
+            { 
+                c1 = 1; 
+                valida = 1; 
+            }
+            else if (escolha == 2 && c2 == 0) 
+            { 
+                c2 = 1; 
+                valida = 1; 
+            }
+            else if (escolha == 3 && c3 == 0) 
+            { 
+                c3 = 1; 
+                valida = 1; 
+            }
+            else if (escolha == 4 && c4 == 0) 
+            { 
+                c4 = 1; 
+                valida = 1; 
+            }
+            else if (escolha == 5 && c5 == 0) 
+            { 
+                c5 = 1; 
+                valida = 1; 
+            }
+
+            if (valida == 1) 
+            {
+                if (escolha == pBotao) 
+                {
+                    printf("VITORIA! Voce achou o botao e escapou!\n");
+                    rodando = 0; 
+                } 
+                else if (escolha == pCobra) 
+                {
+                    printf("DERROTA! A cobra estava ai e te pegou!\n");
+                    rodando = 0; 
+                } 
+                else 
+                {
+                    printf("Ufa... Esta vazia. Passando a vez!\n");
+                    turno++; 
+                }
+            } 
+            else 
+            {
+                printf("Essa escolha e invalida ou a caixa ja esta aberta!\n");
+            }
+
+        } 
+
+        printf("\nDigite 1 para jogar de novo ou 0 para voltar ao menu: ");
         scanf("%d", &jogarNovamente);
-    } while (jogarNovamente == 1);
+
+    } 
+    while (jogarNovamente == 1);
+
+    return 0;
 }
 
-
 // JOGO 3: GOUSMAS WAR 
-
-int jogoGousmasWar() {
+int jogoGousmasWar() 
+{
     int jogarNovamente;
-	int rodando;
-    do {
+    do 
+    {
         printf("\n--- JOGO 3: GOUSMAS WAR ---\n");
-        int f1=1, f2=1, f3=1, f4=1; // Gousmas J1 e J2
-        int turno=1, jogoAtivo=1;
+        
+        int mao1J1 = 1;
+        int mao2J1 = 1;
+        
+        int mao1J2 = 1;
+        int mao2J2 = 1;
 
-        while (rodando == 1) {
-            printf("\nJ1: [%d][%d] | J2: [%d][%d] - Vez: J%d", f1, f2, f3, f4, turno);
-            int acao; printf("\n1-Atacar 2-Dividir: "); scanf("%d", &acao);
+        int turno = 1;
+        int rodando = 1;
 
-            if (acao == 1) {
-                int at, al; printf("Sua Gousma (1-2) no Alvo (1-2): "); scanf("%d %d", &at, &al);
-                if (turno == 1) {
-                    if (at==1 && f1>0 && al==1 && f3>0) f3+=f1;
-                    else if (at==1 && f1>0 && al==2 && f4>0) f4+=f1;
-                    else if (at==2 && f2>0 && al==1 && f3>0) f3+=f2;
-                    else if (at==2 && f2>0 && al==2 && f4>0) f4+=f2;
-                    else { printf("Invalido!"); continue; }
-                } else {
-                    if (at==1 && f3>0 && al==1 && f1>0) f1+=f3;
-                    else if (at==1 && f3>0 && al==2 && f2>0) f2+=f3;
-                    else if (at==2 && f4>0 && al==1 && f1>0) f1+=f4;
-                    else if (at==2 && f4>0 && al==2 && f2>0) f2+=f4;
-                    else { printf("Invalido!"); continue; }
+        while (rodando == 1) 
+        {
+            printf("\n------------------------------");
+            printf("\nJ1: [%d][%d] | J2: [%d][%d]", mao1J1, mao2J1, mao1J2, mao2J2);
+            printf("\nVez do Jogador: %d", turno);
+            
+            int acao; 
+            printf("\n1 - Atacar");
+            printf("\n2 - Dividir Pontos");
+            printf("\nEscolha sua acao: "); 
+            scanf("%d", &acao);
+
+            
+            if (acao == 1) 
+            {
+                int qualMaoMinha, qualMaoAlvo;
+                printf("Qual mao voce vai usar? (1 ou 2): ");
+                scanf("%d", &qualMaoMinha);
+                printf("Qual mao do alvo quer atacar? (1 ou 2): ");
+                scanf("%d", &qualMaoAlvo);
+
+                if (turno == 1) 
+                {
+
+                    if (qualMaoMinha == 1 && mao1J1 > 0 && qualMaoAlvo == 1 && mao1J2 > 0) 
+                    {
+                        mao1J2 = mao1J2 + mao1J1;
+                    }
+                    else if (qualMaoMinha == 1 && mao1J1 > 0 && qualMaoAlvo == 2 && mao2J2 > 0) 
+                    {
+                        mao2J2 = mao2J2 + mao1J1;
+                    }
+                    else if (qualMaoMinha == 2 && mao2J1 > 0 && qualMaoAlvo == 1 && mao1J2 > 0) 
+                    {
+                        mao1J2 = mao1J2 + mao2J1;
+                    }
+                    else if (qualMaoMinha == 2 && mao2J1 > 0 && qualMaoAlvo == 2 && mao2J2 > 0) 
+                    {
+                        mao2J2 = mao2J2 + mao2J1;
+                    }
+                    else 
+                    {
+                        printf("\nAcao invalida! Tente de novo.\n");
+                        continue; 
+                    }
+                } 
+                else 
+                {
+                    if (qualMaoMinha == 1 && mao1J2 > 0 && qualMaoAlvo == 1 && mao1J1 > 0) 
+                    {
+                        mao1J1 = mao1J1 + mao1J2;
+                    }
+                    else if (qualMaoMinha == 1 && mao1J2 > 0 && qualMaoAlvo == 2 && mao2J1 > 0) 
+                    {
+                        mao2J1 = mao2J1 + mao1J2;
+                    }
+                    else if (qualMaoMinha == 2 && mao2J2 > 0 && qualMaoAlvo == 1 && mao1J1 > 0) 
+                    {
+                        mao1J1 = mao1J1 + mao2J2;
+                    }
+                    else if (qualMaoMinha == 2 && mao2J2 > 0 && qualMaoAlvo == 2 && mao2J1 > 0) 
+                    {
+                        mao2J1 = mao2J1 + mao2J2;
+                    }
+                    else 
+                    {
+                        printf("\nAcao invalida! Tente de novo.\n");
+                        continue; 
+                    }
                 }
-            } else if (acao == 2) {
-                int doa, pts; printf("Doadora (1-2) e Pontos: "); scanf("%d %d", &doa, &pts);
-                if (turno==1) {
-                    if (doa==1 && f1>pts && pts>0) { f1-=pts; f2+=pts; }
-                    else if (doa==2 && f2>pts && pts>0) { f2-=pts; f1+=pts; }
-                    else { printf("Erro!"); continue; }
-                } else {
-                    if (doa==1 && f3>pts && pts>0) { f3-=pts; f4+=pts; }
-                    else if (doa==2 && f4>pts && pts>0) { f4-=pts; f3+=pts; }
-                    else { printf("Erro!"); continue; }
+            } 
+            else if (acao == 2) 
+            {
+                int doar, pontos; 
+                printf("Mao doadora (1 ou 2) e quantos pontos passar: "); 
+                scanf("%d %d", &doar, &pontos);
+
+                if (turno == 1) 
+                {
+                    if (doar == 1 && mao1J1 > pontos && pontos > 0) 
+                    { 
+                        mao1J1 = mao1J1 - pontos; 
+                        mao2J1 = mao2J1 + pontos; 
+                    }
+                    else if (doar == 2 && mao2J1 > pontos && pontos > 0) 
+                    { 
+                        mao2J1 = mao2J1 - pontos; 
+                        mao1J1 = mao1J1 + pontos; 
+                    }
+                    else 
+                    { 
+                        printf("\nNao pode dividir assim!\n"); 
+                        continue; 
+                    }
+                } 
+                else 
+                {
+                    if (doar == 1 && mao1J2 > pontos && pontos > 0) 
+                    { 
+                        mao1J2 = mao1J2 - pontos; 
+                        mao2J2 = mao2J2 + pontos; 
+                    }
+                    else if (doar == 2 && mao2J2 > pontos && pontos > 0) 
+                    { 
+                        mao2J2 = mao2J2 - pontos; 
+                        mao1J2 = mao1J2 + pontos; 
+                    }
+                    else 
+                    { 
+                        printf("\nNao pode dividir assim!\n"); 
+                        continue; 
+                    }
                 }
             }
-            if(f1>5)f1=0; if(f2>5)f2=0; if(f3>5)f3=0; if(f4>5)f4=0;
-            if ((f1==0 && f2==0) || (f3==0 && f4==0)) {
-                printf("\nFIM DE JOGO! J1:[%d][%d] J2:[%d][%d]", f1, f2, f3, f4);
-                jogoAtivo = 0;
-            } else turno = (turno == 1) ? 2 : 1;
+
+            if (mao1J1 >= 5) { mao1J1 = 0; }
+            if (mao2J1 >= 5) { mao2J1 = 0; }
+            if (mao1J2 >= 5) { mao1J2 = 0; }
+            if (mao2J2 >= 5) { mao2J2 = 0; }
+
+            if ((mao1J1 == 0 && mao2J1 == 0) || (mao1J2 == 0 && mao2J2 == 0)) 
+            {
+                printf("\n--- FIM DE JOGO! ---");
+                printf("\nResultado final - J1:[%d][%d] J2:[%d][%d]", mao1J1, mao2J1, mao1J2, mao2J2);
+                rodando = 0;
+            } 
+            else 
+            {
+                // Troca o turno
+                if (turno == 1) 
+                {
+                    turno = 2;
+                }
+                else 
+                {
+                    turno = 1;
+                }
+            }
         }
-        printf("\n1: Reiniciar | 0: Menu: "); scanf("%d", &jogarNovamente);
+
+        printf("\nDigite 1 para reiniciar ou 0 para o menu: "); 
+        scanf("%d", &jogarNovamente);
+
     } while (jogarNovamente == 1);
+    
+    return 0;
 }
